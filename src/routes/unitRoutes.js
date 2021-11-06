46e838d3115c118812dedc4b7129329cba12246b
Sina <shavan4s@gmail.com>
2021-11-06 10:07:41 +0330

    555

--- src/routes/unitRoutes.js
+++ src/routes/unitRoutes.js
@@ -15,7 +15,7 @@ router.get("/units/:building", async (req, res) => {
 });
 
 router.post("/units", async (req, res) => {
-  const { name, building, hasOccupant } = req.body;
+  const { name, building } = req.body;
 
   if (!name) {
     return res.status(422).send({ error: "You must fill all forms!" });
@@ -25,7 +25,6 @@ router.post("/units", async (req, res) => {
     const unit = new Unit({
       name,
       building,
-      hasOccupant,
     });
     await unit.save();
     res.send(unit);
